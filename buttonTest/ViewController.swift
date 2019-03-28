169385ae8e31222d21e1f5e6e536a3cfa8af6509
D7703_22 <d7703_22@promise.local>
2019-03-28 12:52:22 +0900

    commit

--- buttonTest/ViewController.swift
+++ buttonTest/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func red(_ sender: Any) {
+        self.view.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func Yellow(_ sender: Any) {
+        self.view.backgroundColor = UIColor.yellow
+    }
+    
+    @IBAction func green(_ sender: Any) {
+        self.view.backgroundColor = UIColor.green
+    }
+    
 
 }
 
